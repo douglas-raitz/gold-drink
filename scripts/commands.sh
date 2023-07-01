e9de8fe1f125aad4897691b53ecacb3c374ad2f9
douglas-dell <douglas_raitz@hotmail.com>
2023-06-30 22:05:44 -0300

    Adicionando estrutura base do projeto, docker-compose, DockerFile, venv, requirements, commands.sh

---
+++ scripts/commands.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# O shell irá encerrar a execução do script quando um comando falhar
+set -e
+
+while ! nc -z $POSTGRES_HOST $POSTGRES_PORT; do
+    echo "Waiting for Postfres Database Startup ($POSTGRES_HOST $POSTGRES_PORT) ..."
+    sleep 2
+done
+
+echo "Postgres Database Started Successfully ($POSTGRES_HOST:$POSTGRES_PORT)"
+
+python manage.py collectstatic --noinput
+python manage.py makemigrations --noinput
+python manage.py migrate --noinput
+python manage.py runserver 0.0.0.0:8000
